26dc74ae048e23b1872100c0362e6dc35f9eed7e
Gabo1794 <sonic1794@gmail.com>
2022-03-22 00:03:09 -0600

    Se realiza maquetado de vista de dias del mes

--- screens/devotionalScreens/components/MonthDaysDevotional.jsx
+++ screens/devotionalScreens/components/MonthDaysDevotional.jsx
@@ -2,7 +2,37 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 const MonthDaysDevotional = () => {
-  return <View></View>;
+  const calendar = () => {
+    const data = [];
+    for (let i = 1; i <= 31; i++) {
+      data.push(
+        <Text style={i === 22 ? [styles.textDay, styles.textToDay] : styles.textDay} key={i}>
+          {i}
+        </Text>
+      );
+    }
+    return data;
+  };
+
+  return <View style={styles.container}>{calendar()}</View>;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    alignContent: "center",
+  },
+  textDay: {
+    width: "20%",
+    textAlign: "center",
+    height: 50,
+    textAlignVertical: "center",
+  },
+  textToDay: {
+    fontWeight: "bold",
+  },
+});
 
 export default MonthDaysDevotional;
