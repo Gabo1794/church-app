8e61bd4c7ea18f98f3db7e91606515374e66db04
Gabo1794 <sonic1794@gmail.com>
2022-03-22 00:22:00 -0600

    se maqueta vista de versiculos del día

--- screens/devotionalScreens/components/DayVersesDevotional.jsx
+++ screens/devotionalScreens/components/DayVersesDevotional.jsx
@@ -1,8 +1,47 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Text, Card, Button, Icon } from "react-native-elements";
 
 const DayVersesDevotional = () => {
-  return <View></View>;
+  return (
+    <View>
+      <Card>
+        <Card.Title>Lunes 22 de Marzo del 2022</Card.Title>
+        <Card.Divider />
+        <View key={1} style={styles.user}>
+          <Text style={styles.name}>Juan 3:16</Text>
+          <Text style={styles.name}>
+            Porque de tal manera amó Dios al mundo, que ha dado a su Hijo
+            unigénito, para que todo aquel que en él cree no se pierda, sino que
+            tenga vida eterna. »Porque de tal manera amó Dios al mundo, que ha
+            dado a su Hijo unigénito para que todo aquel que en él cree no se
+            pierda mas tenga vida eterna.
+          </Text>
+        </View>
+      </Card>
+    </View>
+  );
 };
 
-export default DayVersesDevotional
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  fonts: {
+    marginBottom: 8,
+  },
+  user: {
+    marginBottom: 6,
+  },
+  image: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
+  },
+  name: {
+    fontSize: 16,
+    marginTop: 5,
+  },
+});
+
+export default DayVersesDevotional;
